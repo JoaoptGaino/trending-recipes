dd9b0b48cb6294fbc7103e3dcd178c49fda62c62
joaoptgaino <joaoptgaino@hotmail.com>
2024-09-10 18:31:00 -0300

    first commit

---
+++ src/main/java/com/br/joaoptgaino/trending_recipes/TrendingRecipesApplication.java
@@ -0,0 +1,13 @@
+package com.br.joaoptgaino.trending_recipes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrendingRecipesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrendingRecipesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/joaoptgaino/trending_recipes/TrendingRecipesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.joaoptgaino.trending_recipes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrendingRecipesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
